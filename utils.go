da45b8bdc717ea7a7d71326fa1b57a89b68811b8
tonly <tonly@163.com>
2023-09-28 10:35:34 +0800

    fix: xsql

---
+++ utils.go
@@ -0,0 +1,34 @@
+package xsql
+
+import (
+	"database/sql"
+	"unsafe"
+)
+
+// bytesToString []byte转string
+func bytesToString(b []byte) string {
+	return unsafe.String(&b[0], len(b))
+}
+
+// stringToBytes string 转[]byte
+func stringToBytes(s string) []byte {
+	return unsafe.Slice(unsafe.StringData(s), len(s))
+}
+
+func genEntity(length int) []any {
+	entity := make([]any, 0, length)
+	for i := 0; i < length; i++ {
+		entity = append(entity, new(sql.RawBytes))
+	}
+
+	return entity
+}
+
+func genRecord(data []any, fields []string) map[string]any {
+	record := make(map[string]any, len(fields))
+	for k, v := range data {
+		record[fields[k]] = bytesToString(*v.(*sql.RawBytes))
+	}
+
+	return record
+}
---
+++ xsql_test.go
@@ -0,0 +1,33 @@
+package xsql_test
+
+import (
+	"fmt"
+	"testing"
+	"xsql"
+)
+
+func TestZeroGroup(t *testing.T) {
+	dbConfig := &xsql.Config{
+		Host:     "127.0.0.1",
+		Port:     3306,
+		UserName: "root",
+		Password: "",
+		DBName:   "test",
+		Charset:  "utf8",
+	}
+
+	db := xsql.NewXSQL(dbConfig)
+	//if err := db.Connect(dbConfig); err != nil {
+	//	fmt.Println("err:::::::::::", err)
+	//}
+
+	//db.Table("bag_0000").Primary("uid").Fields("uid", "item", "expire", "itime").Where("uid in (6,8)")
+	//rawsql := db.GenRawSQL()
+	//fmt.Println("rawsql:::::::", rawsql)
+
+	//data, err := db.Table("bag_0000").Primary("uid").Fields("uid", "item", "expire", "itime").Where("uid in (6,8)").Query()
+	data, err := db.Table("bag_0000").Primary("uid").Fields("item", "expire", "itime").Where("uid in (6)").QueryRow()
+	fmt.Println("err:::::::", err)
+	fmt.Println("data:::::::", data)
+
+}
